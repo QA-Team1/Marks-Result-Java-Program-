9a7998c34ffbd9e33f06c7d1b66c679de1010228
17782 <17782@10.0.0.110>
2022-09-02 07:19:25 -0700

    Marks Result Commit

---
+++ src/MarksResult.java
@@ -0,0 +1,56 @@
+
+public class MarksResult {
+
+	public static void main(String[] args) 
+	{
+		int M = 80;
+		int P = 90;
+		int C = 86;
+	
+		System.out.println(M+P+C);
+		System.out.println((M+P+C)/3);
+		
+		if (M>P)
+		{
+			if(M>C)
+			
+			
+			{
+				System.out.println("M = Highest");
+			}
+			else
+			{
+				System.out.println("C= Highest");
+				
+			}
+			}
+			else if(P>C)
+			{
+				System.out.println("P= Highest");
+			}
+			else
+			{System.out.println("C= Highest");
+
+			}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
